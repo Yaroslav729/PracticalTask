21057a789b24856e46cf2e0d19ce103eeb861cd8
Yaroslav Shishkin <company-rosstan@yandex.ru>
2023-05-22 00:24:00 +0300

    TaskTen

---
+++ TaskTen/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int a = int.Parse(Console.ReadLine()!);
+int b = 0;
+if (a < 100 || a > 1000)
+Console.Write("Вы ввели не трёхзначное число");
+else
+
+Console.Write($"{b = a / 10 % 10}");
