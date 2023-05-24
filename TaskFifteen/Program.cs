43a1f19ed6b1a09b2f987a2c0a3ec5f987bf35d5
Yaroslav Shishkin <company-rosstan@yandex.ru>
2023-05-24 23:30:03 +0300

    TaskFifteen

---
+++ TaskFifteen/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Напишите число от 1 до 7: ");
+int num = int.Parse(Console.ReadLine()!);
+if (num >= 1 && num <= 5)
+Console.Write("Нет");
+ else if (num == 6 || num == 7)
+Console.Write("Да");
+else 
+Console.Write("Введите корректные данные");
