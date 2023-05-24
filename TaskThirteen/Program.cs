45adb22b4afca687d7a2a1b443a9047bb3e8f3a0
Yaroslav Shishkin <company-rosstan@yandex.ru>
2023-05-24 22:06:22 +0300

    taskFhirteen

---
+++ TaskThirteen/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+Console.Write("Введите число: ");
+string str = Console.ReadLine()!;
+char[] arr = new char[str.Length];
+
+    if (arr.Length > 2)
+    {
+        for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = str[i];         
+            }
+       }     
+            
+ else
+    {
+        Console.WriteLine("Введите трёхзначное число");
+    }
+
+    Console.WriteLine(arr[2]);
+
